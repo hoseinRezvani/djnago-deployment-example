09cc0f71063c681a881d1b4b550f6c67ee6829cc
hosein rezvani <hrezvani78@gmail.com>
2021-04-02 10:48:09 +0430

    'first'

---
+++ templatelearning/basic_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context = {'name': 'hosein', 'age': 22}
+    return render(request, 'basic_app/index.html', context)
+
+def other(request):
+    return render(request, 'basic_app/other.html')
+
+def relative(request):
+    return render(request, 'basic_app/relative_url_templates.html')
